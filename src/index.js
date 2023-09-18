a4b54305e9dbe89f40eba65ca3d8c8e3bf7d2835
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-09-18 13:03:57 -0400

    updated the DOM with information

--- src/index.js
+++ src/index.js
@@ -21,4 +21,33 @@ const imageContainer = document.getElementById('toy-collection')
 
 fetch(toyImageUrl)
   .then((response) => response.json())
-  .then((data) =>console.log(data))
+  .then((data) =>data.forEach((toyObject)=>{
+    //grab div with id = toy collection
+    const toyContainer = document.getElementById("toy-collection")
+    //create h2 
+    //create a div with 
+    const newDiv = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    img.setAttribute('class','toy-avatar')
+    img.alt = toyObject.name
+    const para = document.createElement('p')
+    const button = document.createElement('button')
+    button.setAttribute('class','like-btn')
+    button.setAttribute('id',toyObject.id )
+    newDiv.append(h2,img,para,button)
+    toyContainer.append(newDiv)
+    h2.innerText = toyObject.name
+    img.src = toyObject.image 
+    para.innerText = toyObject.likes
+  
+
+
+    
+  
+
+  }))
+//create h2 
+//create img tag, p tag ,button tag 
+//use foreach method to go through array 
+
